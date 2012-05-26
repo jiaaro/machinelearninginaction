b2d15911d4d45f4c1f75d4618a8721da58da5b30
James Robert <jiaaro@gmail.com>
2012-05-26 16:42:02 -0400

    majority count more pythonic

--- Ch03/trees.py
+++ Ch03/trees.py
@@ -79,12 +79,8 @@ def chooseBestFeatureToSplit(dataSet):
     return max(features, key=infoGain)
 
 def majorityCnt(classList):
-    classCount={}
-    for vote in classList:
-        if vote not in classCount.keys(): classCount[vote] = 0
-        classCount[vote] += 1
-    sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), reverse=True)
-    return sortedClassCount[0][0]
+    classCount = Counter(classList)
+    return max(classCount, key=lambda key: classCount[key])
 
 def createTree(dataSet,labels):
     classList = [example[-1] for example in dataSet]
