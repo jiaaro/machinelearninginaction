50c299d381c420a41c7c2a8b70b3aab0837343eb
James Robert <jiaaro@gmail.com>
2012-05-26 16:38:47 -0400

    add notes on what entropy is, and make the chooseBestFeatureToSplit function more pythonic

--- Ch03/trees.py
+++ Ch03/trees.py
@@ -2,7 +2,25 @@
 Created on Oct 12, 2010
 Decision Tree Source Code for Machine Learning in Action Ch. 3
 @author: Peter Harrington
+
+Things to note:
+    Entropy is a measurement of "chaos in a set"
+    
+    Example high entropy set:
+    high_entropy_set = set("dog", "cat", "bird", "fish", "lizard")
+    
+    Example low entropy set:
+    low_entropy_set = set("dog", "cat", "dog", "dog", "cat")
+    
+    Example 0 entropy set:
+    zero_entropy_set = set("dog", "dog", "dog", "dog", "dog")
+    
+    Each set has the same number of values, but the low entropy set has less 
+    "chaos". The last set is uniform (all values are the same) so it has an
+    entropy of 0.
+    
 '''
+
 from math import log
 import operator
 from collections import Counter
@@ -32,31 +50,33 @@ def calcShannonEnt(dataSet):
     return sum(entropy(labelCount) for labelCount in labelCounts.values())
     
 def splitDataSet(dataSet, axis, value):
-    retDataSet = []
-    for featVec in dataSet:
-        if featVec[axis] == value:
-            reducedFeatVec = featVec[:axis]     #chop out axis used for splitting
-            reducedFeatVec.extend(featVec[axis+1:])
-            retDataSet.append(reducedFeatVec)
-    return retDataSet
+    return [ 
+        # chop out axis used for splitting:
+        featVec[:axis] + featVec[axis+1:]
+        
+        for featVec in dataSet
+        if featVec[axis] == value ]
     
 def chooseBestFeatureToSplit(dataSet):
-    numFeatures = len(dataSet[0]) - 1      #the last column is used for the labels
+    # The last column is used for the labels
+    numFeatures = len(dataSet[0]) - 1
     baseEntropy = calcShannonEnt(dataSet)
-    bestInfoGain = 0.0; bestFeature = -1
-    for i in range(numFeatures):        #iterate over all the features
-        featList = [example[i] for example in dataSet]#create a list of all the examples of this feature
-        uniqueVals = set(featList)       #get a set of unique values
+    
+    def infoGain(featureToSplitOn):
+        # get a set of unique values for this feature
+        uniqueVals = set(example[i] for example in dataSet)    
+        
         newEntropy = 0.0
         for value in uniqueVals:
             subDataSet = splitDataSet(dataSet, i, value)
-            prob = len(subDataSet)/float(len(dataSet))
-            newEntropy += prob * calcShannonEnt(subDataSet)     
-        infoGain = baseEntropy - newEntropy     #calculate the info gain; ie reduction in entropy
-        if (infoGain > bestInfoGain):       #compare this to the best gain so far
-            bestInfoGain = infoGain         #if better than current best, set to best
-            bestFeature = i
-    return bestFeature                      #returns an integer
+            prob = len(subDataSet) / float(len(dataSet))
+            newEntropy += prob * calcShannonEnt(subDataSet)
+            
+        # calculate the info gain; ie reduction in entropy
+        return baseEntropy - newEntropy
+    
+    features = range(numFeatures)
+    return max(features, key=infoGain)
 
 def majorityCnt(classList):
     classCount={}
